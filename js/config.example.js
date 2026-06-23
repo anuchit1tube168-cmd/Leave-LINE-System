4fa6ce2e32360fa7ff1a23300d1dab67f03e7e39
AGISLAMIOUS <anuchit1tube168@gmail.com>
2026-06-23 19:28:51 +0700

    Add frontend config example

---
+++ js/config.example.js
@@ -0,0 +1,11 @@
+window.LEAVE_SYSTEM_CONFIG = {
+  apiUrl: '',
+  liffId: '',
+  approvalLevels: 2,
+  demoMode: true
+};
+
+// วิธีใช้งานจริง:
+// 1) copy เป็น js/config.js
+// 2) ใส่ apiUrl จาก Google Apps Script Web App
+// 3) ใส่ liffId จาก LINE Developers Console
